e175ae0d48b35e98c1e26abe7a6c472809d32c8f
RoRaut <rohinibraut20@gmail.com>
2024-06-13 12:20:12 +0530

    data types variables

---
+++ .devcontainer/01_BASICS/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 3423423 //cannt change
+let email="abc@gmail.com"
+var accPassword="1234"
+accountCity="Pandharpur"
+
+//accountId = 3 //not allowed
+
+email = "pqr@gmail.com"
+accPassword ="32423"
+accountCity ="Pune"
+
+let accountState;
+
+console.log(accountState);
+
+console.log(accountId, email, accPassword, accountCity)
+
+console.table([accountId, email, accPassword, accountCity])
---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,24 @@
+"new strict"; // treat all JS code as newer version
+
+// alert(7+7) // we are using nodejs not brower
+
+//data types
+let name = "Rohini"
+let age = 23
+let isLoggedIn = true
+let state=null
+
+//primitive data types
+// bigint
+//string
+//number
+//boolean
+//null => standalone value
+//undefined => value not assigned 
+//symbol => uniquness
+
+console.log(typeof(age)); //number
+console.log(typeof null); //object
+
+//objects
+
