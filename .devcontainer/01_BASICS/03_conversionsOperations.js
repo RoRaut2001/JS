d9f1a814521ef1f9fd230680d9b878ffc8a500d5
RoRaut <rohinibraut20@gmail.com>
2024-06-13 12:50:50 +0530

    convesion of data types

---
+++ .devcontainer/01_BASICS/03_conversionsOperations.js
@@ -0,0 +1,29 @@
+// let score=35
+// console.log(typeof score);
+
+
+// let valueInNumber= Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let score1="34abc" //not pure number =>NaN (Not a Number)
+
+// let valueInNumber1=Number(score1)
+// console.log(valueInNumber1);
+// console.log(typeof valueInNumber1);
+
+//score1= null
+//console.log(score1);
+
+let isLogged =1
+let booleanIslogged = Boolean(isLogged)
+console.log(booleanIslogged)
+
+//1=> true 0=> false
+//" "=>false
+//"rohini" =>true
+
+let someNumber= 2001
+let string =String(someNumber)
+console.log(string);
+console.log(typeof string);
