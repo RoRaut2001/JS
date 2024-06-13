c63cfdace17b3ebd3cc58020ee9cec4ea1dae9a7
RoRaut <rohinibraut20@gmail.com>
2024-06-13 15:21:12 +0530

    datatypeSummary

--- .devcontainer/01_BASICS/04_comparison.js
+++ .devcontainer/01_BASICS/04_comparison.js
@@ -6,4 +6,6 @@
 // console.log(null>=0);
 // console.log(null<=0);
 
+//===  stricktly check
+
 console.log("2"==="2")
---
+++ .devcontainer/dataTypesSummary.js
@@ -0,0 +1,52 @@
+// data types are based on the how it is stored in memory and how to access it
+// primitive and non primitive data types
+
+//primitive  --7
+// string, number, boolean, null, undefined, Symbol, BigInt
+
+
+// return type of val
+// undefined--> undefined
+// Null --> Object
+
+// reference type: non primitive 
+// arrays, objects, functions
+
+
+// java script is dynamically typed or statically typed language??
+// dynamically typed language
+
+const score=100
+const scoreValue= 100.4
+
+const isLoggedIn= true
+const outsideTemp= null
+let userEmail;
+
+const id=Symbol("123")
+const id2=Symbol("123")
+console.log(id);
+console.log("id2");
+console.log(id2);
+
+console.log(id == id2);
+//Two Symbols with the same description are not equal because they are different unique instances.
+
+const bigNumber = 3483296429734863278642387n //n -> convert number into bigInt
+console.log(bigNumber);
+
+// non primitive
+
+
+const heros=["sk","pk","gk"]; //array
+let myObj={ 
+    name:"Rohini",
+    age: 22
+} // object
+
+const myFun=function(){
+    console.log("Hello");
+}
+
+console.log(typeof myFun) //object function 
+console.log(typeof id);
