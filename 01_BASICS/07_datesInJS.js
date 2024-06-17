4ee7faceff18edc192bc2b6bb0fc3bd94de0597f
RoRaut <rohinibraut20@gmail.com>
2024-06-17 12:58:32 +0530

    date time

---
+++ 01_BASICS/07_datesInJS.js
@@ -0,0 +1,16 @@
+
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+
+let myDate = new Date(2001,9 , 20, 10, 22)
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+//timestamps
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
