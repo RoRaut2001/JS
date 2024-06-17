4330b522da83783da28edf690566285d684bbf5a
RoRaut <rohinibraut20@gmail.com>
2024-06-17 12:37:30 +0530

    number and maths

---
+++ 01_BASICS/06_number_and_maths.js
@@ -0,0 +1,29 @@
+// const score = 100
+// console.log(typeof score);
+
+const balance = new Number(2000)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 4223.95445
+// console.log(otherNumber.toPrecision(6));
+
+const hunderd= 100000
+// console.log(hunderd.toLocaleString('en-IN'));
+// max_value, min_value
+
+//maths library
+
+// console.log(Math);
+// console.log(Math.abs(-40));
+// console.log(Math.round(30.43));
+// console.log(Math.ceil(30.43));
+// console.log(Math.floor(30.43));
+console.log(Math.random()); // always random values between 0-1, decimal value always
+console.log((Math.random()*10 )+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max= 20
+console.log(Math.floor(Math.random()*(max-min))+10);
